ee7908df6113f910e40a47a07122ac0c1f4b20d6
David De La Torre <datocam98@gmail.com>
2022-09-20 18:40:34 +0200

    primer commit

---
+++ application/views/nota/c.php
@@ -0,0 +1,31 @@
+<div class="container">
+	<h1>Nueva nota</h1>
+
+	<form action="<?=base_url()?>nota/cPost" method="post">
+		
+		<fieldset>
+		<legend>Alumno</legend>
+		<select name="idAlumno">
+			<?php foreach ($alumnos as $alumno):?>
+			<!-- eSTE ES EL VALOR RECOGIDO -->
+				<option value="<?=$alumno->id?>">
+					<?= $alumno->apellido ?>, <?= $alumno->nombre ?>
+				</option>
+			<?php endforeach;?>
+		</select>
+		</fieldset>
+		<br/>
+		<!-- RADIO DE ASIGNATURAS-->
+		<?php foreach ($asignaturas as $asignatura):?>
+			<input id="id-<?=$asignatura->id?>" type="radio" value="<?=$asignatura->id?>" name="idAsignatura" checked="checked"/>
+			<label for="id-<?=$asignatura->id?>"><?=$asignatura->nombre?></label>
+		<?php endforeach;?>
+		<br/>
+	
+		<label for="idNota">Nota</label> 
+		<input id="idNota" type="number" name="numero" required="required" min="0" max="10" value="5"/> 
+		<br /> 
+		
+		<input type="submit" />
+	</form>
+</div>
